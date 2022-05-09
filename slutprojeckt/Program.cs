7e6187a183ed1eb75b40c7ec1e498b21ca308e28
Joen Wilund <vetekatten3@gmail.com>
2022-05-09 08:29:09 +0200

    yeet

---
+++ slutprojeckt/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+int i;
+
+
+{
+for (i = 1; i <= 100; i++) 
+{
+  if (i % 3 == 0){
+      Console.WriteLine("Fizz");
+  }
+    
+
+  if (i % 5 == 0) 
+  {
+      Console.WriteLine("Buzz");
+  }
+
+  if ((i % 3 == 0) && (i % 5 == 0))
+  {
+      Console.WriteLine("FizzBuzz");
+  }
+  else 
+  {
+      Console.WriteLine(i);
+  }
+}
+
+}
+
+
+try
+{
+Console.WriteLine("Nu är det en string");
+String stringNumber=i.ToString();
+Console.WriteLine(stringNumber);
+}
+catch
+{
+Console.WriteLine("Error");
+}
+
+Console.ReadLine();
